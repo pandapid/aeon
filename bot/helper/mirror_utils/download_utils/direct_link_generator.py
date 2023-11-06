5bf0ffdaa6410661d961acb4d3c681d12dafe107
Kuma-Stream <121804937+Kuma-Stream@users.noreply.github.com>
2023-11-06 16:32:59 +0700

    recapcha

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -215,6 +215,25 @@ def onedrive(link):
         raise DirectDownloadLinkException('ERROR: Direct link not found')
     return resp['@content.downloadUrl']
 
+def RecaptchaV3(ANCHOR_URL):
+    url_base = 'https://www.google.com/recaptcha/'
+    post_data = "v={}&reason=q&c={}&k={}&co={}"
+    client = requests.Session()
+    client.headers.update({
+        'content-type': 'application/x-www-form-urlencoded'
+    })
+    matches = re_findall('([api2|enterprise]+)\/anchor\?(.*)', ANCHOR_URL)[0]
+    url_base += matches[0]+'/'
+    params = matches[1]
+    res = client.get(url_base+'anchor', params=params)
+    token = re_findall(r'"recaptcha-token" value="(.*?)"', res.text)[0]
+    params = dict(pair.split('=') for pair in params.split('&'))
+    post_data = post_data.format(params["v"], token, params["k"], params["co"])
+    res = client.post(url_base+'reload',
+                      params=f'k={params["k"]}', data=post_data)
+    answer = re_findall(r'"rresp","(.*?)"', res.text)[0]
+    return answer
+    
 def ouo(url: str) -> str:
     client = requests.Session()
     tempurl = url.replace("ouo.press", "ouo.io")
